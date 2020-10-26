45453aa5e27774f0004b2a14535c2ebc356ccffd
enkhjargal <6816054@muls.edu.mn>
2020-10-26 12:40:12 +0800

    Refresh

--- dadlga.js
+++
@@ -1,9 +0,0 @@
-var mashin = [ 'prius20', 'prius30', 'prius50', 'hhh'] 
-mashin[1] ='prius20';
-mashin.push('mark2')
-//Car.splice(1, 2); ustgah
-// 1-2 hvrtel awna 3orohgui
-// var car = new Array('prius', 'carina')
-document.getElementById("jishee").innerHTML=(mashin);
-document.getElementById("jishee1").innerHTML=(mashin.length);
-
--- jishee.js
+++
@@ -1,2 +0,0 @@
-var mashin = ['prius10', 'prius20']
-document.getElementById("demo").innerHTML=mashin;
